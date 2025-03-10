a4ddc84bb0e04422545ad42fc42ef23328991498
dwirzkid <dwirizki164@gmail.com>
2025-03-10 20:45:47 +0700

    feat: custom button

---
+++ lib/view/widgets/custom_button.dart
@@ -0,0 +1,35 @@
+import 'package:crud_project_1/theme.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final VoidCallback? onPressed;
+
+  const CustomButton({required this.title, this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 40,
+      margin: EdgeInsets.only(top: 50),
+      decoration: BoxDecoration(
+        color: Color(0xffEC7FA9),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
